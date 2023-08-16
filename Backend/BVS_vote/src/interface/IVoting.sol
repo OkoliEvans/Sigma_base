8eba8414a1668848c3245bb1286f3f5779765fc3
Evans <mr.evans0075@gmail.com>
2023-08-16 10:23:05 +0100

    fixing stack too deep error

--- Backend/BVS_vote/src/interface/IVoting.sol
+++ Backend/BVS_vote/src/interface/IVoting.sol
@@ -3,40 +3,27 @@ pragma solidity ^0.8.13;
 
 interface IVoting {
 
-    /// @notice To add vote administrators
-    function addAgent(address agent, string calldata name, uint voteId) external returns(bool);
+    function init2(string memory uri, uint start, uint end) external;
 
-    /// @notice To remove vote administrators
-    function rmAgent(address agent, uint voteId) external returns(bool);
 
     /// @notice To add vote candidates for a post
     function addCandidate(string calldata name, string calldata post, uint id, uint voteId) external returns(bool);
 
     /// @notice To remove vote candidates for a post
-    function rmCandidate(string calldata name, uint voteId) external returns(bool);
+    function rmCandidate(address addr) external returns(address[] memory);
 
     /// @notice To verify voters 
-    function verify(address voter, uint voteId) external returns(bool);
+    function verify(uint vn) external;
 
-    /// @notice To initialise a vote contest
-    function init(uint start, uint end, uint voteId, string calldata contest) external returns(bool);
 
     /// @notice To start voting process
-    function start(uint voteId) external;
+    function startVote() external;
 
     /// @notice To end voting process
-    function end(uint voteId) external;
+    function endVote() external;
 
-    /// @notice To display duration
-    function time(uint voteId) external;
 
     /// @notice To collect votes
-    function vote(uint voteId, string calldata candidate) external returns (bool);
-
-    /// @notice To display results
-    function result(uint voteId) external;
-
-/// @notice for agents to attest for or against the vote
-    function attest() external;
+    function vote(uint voteId, address candidate) external returns (bool);
 
 }
