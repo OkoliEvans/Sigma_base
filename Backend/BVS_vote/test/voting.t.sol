4f808222b50522878ef36607951b5c78a694695a
Evans <mr.evans0075@gmail.com>
2023-08-16 12:47:27 +0100

    fixing stack too deep error

--- Backend/BVS_vote/test/voting.t.sol
+++ Backend/BVS_vote/test/voting.t.sol
@@ -2,7 +2,40 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
+import "../src/contract/voting.sol";
+import "../src/contract/votingFactory.sol";
+import "../src/interface/IVoting.sol";
 
 contract CounterTest is Test {
+    votingFactory votingfactory;
+    address Moderator = 0xc6d123c51c7122d0b23e8B6ff7eC10839677684d;
+    address overseer = 0x49207A3567EF6bdD0bbEc88e94206f1cf53c5AfC;
+
+    function setUp() public {
+        votingfactory = new votingFactory();
+        console2.log("Caller: ", msg.sender);
+    }
+
+    function test_createElection() public {
+        vm.prank(overseer);
+        (address childElection) = votingfactory.createElection(
+            1692189057,
+            1692275457,
+            10,
+            "National Election 2023",
+            "NEN-2023",
+            "https://ipfs.io/ipfs/QmX84bZL51sJ4g4M8XoWQnBWQJ4Fh1TbT8TgfW2yyfNft",
+            "Presidential Election"
+        );
+
+        console2.log(childElection);
+        votingfactory.retElections();
+        votingfactory.retElectionID(childElection);
+
+        votingfactory.replcModerator(Moderator);
+    }
+
+
+    
 
 }
