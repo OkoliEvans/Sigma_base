7365f1e5cef1ed5892e07988f4a9d4b0a46c8616
Evans <mr.evans0075@gmail.com>
2023-08-17 11:43:19 +0100

    fixing stack too deep error

--- Backend/BVS_vote/test/voting.t.sol
+++ Backend/BVS_vote/test/voting.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.21;
 
 import {Test, console2} from "forge-std/Test.sol";
 import "../src/contract/voting.sol";
@@ -19,13 +19,12 @@ contract CounterTest is Test {
     function test_createElection() public {
         vm.prank(overseer);
         (address childElection) = votingfactory.createElection(
-            1692189057,
-            1692275457,
             10,
             "National Election 2023",
             "NEN-2023",
             "https://ipfs.io/ipfs/QmX84bZL51sJ4g4M8XoWQnBWQJ4Fh1TbT8TgfW2yyfNft",
-            "Presidential Election"
+            "Presidential Election",
+            1692275457
         );
 
         console2.log(childElection);
