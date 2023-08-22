425fa51656c18d9d878b8f86d73f6ea604eb159e
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-08-22 20:56:03 +0100

    set-up environment and navbar

---
+++ frontend/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+    address: "/",
+  },
+  {
+    id: "vote",
+    title: "Vote",
+    address: "/vote",
+  },
+  {
+    id: "about",
+    title: "About",
+    address: "/about",
+  },
+  {
+    id: "docs",
+    title: "Docs",
+    address: "/docs",
+  },
+];
