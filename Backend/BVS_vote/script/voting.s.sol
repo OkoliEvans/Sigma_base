4b8b74e69db930b1ea2c5b13b6027dc0a70bebdb
Evans <mr.evans0075@gmail.com>
2023-08-22 08:09:32 +0100

    updated script and env files

--- Backend/BVS_vote/script/voting.s.sol
+++ Backend/BVS_vote/script/voting.s.sol
@@ -13,6 +13,9 @@ contract CounterScript is Script {
         address deployer = 0xc6d123c51c7122d0b23e8B6ff7eC10839677684d;
         address overseer = 0x49207A3567EF6bdD0bbEc88e94206f1cf53c5AfC;
 
+        address baseDeployer = 0x3E8b4D19a2ff39b5d1B73c0D74F54F7Df838987F;
+        
+
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
         uint256 overseerKey = vm.envUint("PRIVATE_KEY3");
 
